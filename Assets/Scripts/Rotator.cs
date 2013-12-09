d692c5f96d9b52f93a508533fda899c9d94ab096
Alexandre Rabérin <kernelith@live.fr>
2013-12-09 23:19:59 +0100

    règle des soucis de collisions avec le lapin. Ajoute un script pour appliquer des rotations aléatoires sur les Carrot et les Key

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	private float xRotation = 0;
+	private float yRotation = 0;
+	private float zRotation = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		xRotation = Random.Range(10, 15);
+		yRotation = Random.Range(10, 15);
+		zRotation = Random.Range(10, 15);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(new Vector3(xRotation, yRotation, zRotation) * Time.deltaTime);
+	}
+}
