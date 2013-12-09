2dfe7dd352bf9636e348173094ef9f2008dd1223
Alexandre Rabérin <kernelith@live.fr>
2013-12-09 09:10:59 +0100

    ajoute le switch entre camera fixe du niveau et camera du joueur

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Camera playerCamera;
+	public Camera fixedLevelCamera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		fixedLevelCamera.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyUp(KeyCode.Tab))
+		{
+			switchCamera();
+		}
+	}
+
+	void switchCamera()
+	{
+		fixedLevelCamera.enabled = !fixedLevelCamera.enabled;
+		playerCamera.enabled = !playerCamera.enabled;
+	}
+}
