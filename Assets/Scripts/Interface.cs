f07ccf0c5829cd086360efde13c4deda993455ce
Alexandre Rabérin <kernelith@live.fr>
2013-12-17 17:09:40 +0100

    ajoute le système pour gérer la barre de vie (et déplace la gestion de l'affichage du game over

--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -9,11 +9,27 @@ public class Interface : MonoBehaviour {
 	// Joueur
 	public PlayerController player;
 
+	// Textures barre de vie
+	public GUITexture healthTextureEmpty;
+	public GUITexture healthTexture;
+
 	// Use this for initialization
 	void Start () 
 	{
 		gameOver.enabled = false;
 		gameOver.text = "Game Over";
+
+		// Placement de la barre de vie (remplissage) => en dehors de l'écran pour subir un offset par la suite pour simuler son mouvement
+		healthTexture.pixelInset = new Rect(-Screen.width/2 + 0.05f*Screen.width - healthTextureEmpty.texture.width,
+		                                    Screen.height/2 - 1.5f*healthTextureEmpty.texture.height,
+		                                    healthTexture.texture.width,
+		                                    healthTexture.texture.height);
+
+		// Placement de la barre de vie (contour)
+		healthTextureEmpty.pixelInset = new Rect(-Screen.width/2 + 0.05f*Screen.width,
+		                                         Screen.height/2 - 1.5f*healthTextureEmpty.texture.height,
+		                                         healthTextureEmpty.texture.width,
+		                                         healthTextureEmpty.texture.height);
 	}
 	
 	// Update is called once per frame
@@ -24,5 +40,14 @@ public class Interface : MonoBehaviour {
 			gameOver.enabled = true;
 			Time.timeScale = 0;
 		}
+	}
+
+	void LateUpdate()
+	{
+		// Actualisation de la barre de vie
+		// en déterminant l'offset en pixel à appliquer à la texture de remplissage
+		int hpBarLenght = (int) (healthTexture.texture.width + 194*((double)player.getHealth()/(double)player.getMaxHealth()));
+
+		healthTexture.border = new RectOffset(hpBarLenght, 0, 0, 0);
 	}
 }
