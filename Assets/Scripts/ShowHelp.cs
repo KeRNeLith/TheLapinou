a631703934b7fd82f9ef162bde6d20b62cb004ea
Alexandre Rabérin <kernelith@live.fr>
2013-12-28 16:27:55 +0100

    modifie le level 1 pour en faire celui de la version finale (et ajoute un prefab pour mettre des box d'aide pour le joueur)

---
+++ Assets/Scripts/ShowHelp.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowHelp : MonoBehaviour 
+{
+	private GUIText message;
+	public TextMesh info;
+
+	// Use this for initialization
+	void Start () 
+	{
+		message = transform.FindChild("Message").guiText;
+		message.enabled = false;
+		info.renderer.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			message.enabled = true;
+			info.renderer.enabled = false;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			message.enabled = false;
+			info.renderer.enabled = true;
+		}
+	}
+}
